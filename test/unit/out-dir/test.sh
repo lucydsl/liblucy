0b5791d5b59bedff1438ba6fa7f990522d133cb7
Matthew Phillips <matthew@matthewphillips.info>
2021-03-23 07:44:19 -0400

    Added a unit test for --out-dir (#3)
    Co-authored-by: Matthew Phillips <matthew@phillipsoft.com>

---
+++ test/unit/out-dir/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dir=$(dirname "$0")
+
+outdir="$dir/test-out"
+srcdir="$dir/src"
+
+# Before
+rm -rf $outdir
+
+# Run compiler
+$LC --out-dir $outdir $srcdir
+
+# Test to see if expected files exist.
+if [ ! -f "$outdir/machine.js" ]; then
+  exit 1
+fi
+
+if [ ! -f "$outdir/nested/machine.js" ]; then
+  exit 1
+fi
+
+exit 0
