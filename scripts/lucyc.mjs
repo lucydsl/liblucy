39e0af2a7793580432c04b32e6f912d382d77f58
Matthew Phillips <matthew@phillipsoft.com>
2021-05-01 08:10:38 -0400

    Adds a test for --remote-imports

--- scripts/lucyc.mjs
+++ scripts/lucyc.mjs
@@ -1,9 +1,29 @@
 #!/usr/bin/env node
 import { promises as fsPromises } from 'fs';
-const { readFile } = fsPromises;
+const { readFile, writeFile } = fsPromises;
 
-const [,, filename] = process.argv;
+let index = 2;
+let [,, filename] = process.argv;
 const env = process.env.NODE_ENV || 'development';
+
+let outFile = null;
+const options = {
+  useRemote: false
+};
+
+while(filename && filename.startsWith('--')) {
+  switch(filename) {
+    case '--remote-imports': {
+      options.useRemote = true;
+      break;
+    }
+    case '--out-file': {
+      outFile = process.argv[++index];
+      break;
+    }
+  }
+  filename = process.argv[++index];
+}
 
 if(!filename) {
   console.error('A filename is required')
@@ -22,9 +42,14 @@ async function run() {
   await ready;
 
   try {
-    const js = compileXstate(contents, filename);
-    process.stdout.write(js);
-    process.stdout.write("\n");
+    const js = compileXstate(contents, filename, options);
+
+    if(outFile) {
+      await writeFile(outFile, js, 'utf-8');
+    } else {
+      process.stdout.write(js);
+      process.stdout.write("\n");
+    }
   } catch {
     process.stderr.write("Compilation failed!\n");
     process.exit(1);
---
+++ test/unit/remote_imports/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dir=$(dirname "$0")
+
+ret=0
+input="$dir/input.lucy"
+expected="$dir/expected.js"
+
+# Before
+tmp=$(mktemp)
+
+# Run compiler
+$LC --remote-imports --out-file $tmp $input
+
+d=$(diff $expected $tmp | colordiff)
+
+if [ ${#d} -ge 1 ]; then
+  echo -e "${red}FAILED${nc} - $input"
+  echo ""
+  echo "$d"
+
+  ret=1
+fi
+
+exit $ret
